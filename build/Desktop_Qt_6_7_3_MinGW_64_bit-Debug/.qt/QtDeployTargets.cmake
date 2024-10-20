92c35ec8eed2994728f47e2efc85b36f661fc58c
vinamrakumarjha <vinamrakumarjha@gmail.com>
2024-10-20 21:26:52 +0530

    removed build directory

--- build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_appWhiteBoard_FILE E:/Qt_CMakeProject/WhiteBoard/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/appWhiteBoard.exe)
-set(__QT_DEPLOY_TARGET_appWhiteBoard_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_appWhiteBoard_RUNTIME_DLLS C:/Qt/6.7.3/mingw_64/bin/Qt6Quick.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Network.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
